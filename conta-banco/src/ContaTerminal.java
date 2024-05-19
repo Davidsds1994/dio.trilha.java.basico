861cca2766656794dd1862a69b4c3d076163ff1f
davidsds1994 <ryanzikinha13@gmail.com>
2024-05-18 22:30:37 -0300

    DioBanco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,35 +1,3 @@
-import java.util.Scanner;
-
-public class ContaTerminal {
-
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int numero;
-    String agencia;
-    String nomeCliente;
-    double saldo;
-
-    System.out.println("Conta");
-    System.out.println();
-    System.out.println("Por Favor, Digite o numero da Conta: (1234)");
-    numero =sc.nextInt();
-    
-           System.out.println("Digite o Numero da Sua Agencia (123-4)");
-           agencia =sc.next();
-
-        System.out.println("Informe seu Nome");
-        nomeCliente =sc.next();
-        sc.nextLine();
-
-        System.out.println("Informe se Saldo");
-        saldo = sc.nextDouble();
-
-        System.out.println();
-        System.out.printf("olá $, sua conta foi criada com sucesso"
-      + "sua agencia é $" + "Sua conta é $" + "seu saldo $ ja esta dispovinel para o saque",
-      nomeCliente, agencia, numero, saldo);
-        sc.close();
 
 
-  }
-  }  
+
